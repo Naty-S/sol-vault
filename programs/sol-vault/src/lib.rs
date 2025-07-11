006a50129bf76cf5cd5c778e8d3ea4cb53b71ccd
“Naty-S” <na7a5cha010@gmail.com>
2025-07-10 22:14:02 -0400

    Add withdraw

--- programs/sol-vault/src/lib.rs
+++ programs/sol-vault/src/lib.rs
@@ -22,6 +22,12 @@ pub mod sol_vault {
         
         ctx.accounts.deposit(amount)
     }
+
+    // Withdraw instruction
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
+        
+        ctx.accounts.withdraw(amount)
+    }
 }
 
 #[derive(Accounts)]
@@ -118,6 +124,58 @@ impl<'info> Deposit<'info> {
         };
 
         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        transfer(cpi_ctx, amount)
+    }
+}
+
+#[derive(Accounts)]
+// Withdraw context
+pub struct Withdraw<'info> {
+    
+    #[account(mut)]
+    pub user: Signer<'info>,
+    
+    #[account(
+        seeds = [b"state", user.key().as_ref()],
+        bump = vault_state.state_bump, // already calculated at init
+    )]
+    pub vault_state: Account<'info, VaultState>,
+    
+    #[account(
+        mut, // 'cuz deducting lamports (SOL)
+        seeds = [b"vault", vault_state.key().as_ref()], 
+        bump = vault_state.vault_bump,
+    )]
+    pub vault: SystemAccount<'info>,
+    
+    // Allows the lamport transfer
+    pub system_program: Program<'info, System>,
+}
+
+// Withdraw context functions
+impl<'info> Withdraw<'info> {
+    pub fn withdraw(&mut self, amount: u64) -> Result<()> {
+
+        let cpi_program = self.system_program.to_account_info();
+
+        let cpi_accounts = Transfer {
+            from: self.vault.to_account_info(),
+            to: self.user.to_account_info(),
+        };
+
+        // Create seeds for the context to know the signer
+        // Use the vault (from) seeds for derivation (in same order) and bump
+        let seeds = &[
+            b"vault",
+            self.vault_state.to_account_info().key.as_ref(),
+            &[self.vault_state.vault_bump],
+        ];
+
+        // Needed to sign the ix
+        let signer_seeds = &[&seeds[..]];
+
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
 
         transfer(cpi_ctx, amount)
     }
